5fd676578604979fec3b288e4fa602a3c757cb2e
Berkay Atatop <batatop@gmail.com>
2018-06-21 01:29:28 -0400

    designed login page

---
+++ src/assets/styles/colors.js
@@ -0,0 +1,22 @@
+// https://material.io/tools/color/#!/?view.left=0&view.right=0&primary.color=757575&secondary.color=B71C1C
+
+// Background Colors
+export const appBackground = '#fffafa'
+
+// Primary Colors
+export const primary = '#757575'
+export const primaryLight = '#a4a4a4'
+export const primaryDark = '#494949'
+
+// Secondary Colors
+export const secondary = '#b71c1c'
+export const secondaryLight = '#f05545'
+export const secondaryDark = '#7f0000'
+
+// Text Colors
+export const lightText = '#ffffff'
+export const darkText = '#000000'
+export const noteText = '#a9a9a9'
+
+// Title Color
+export const appTitle = '#3B3C36'
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Text, TextInput, TouchableHighlight, View } from 'react-native';
+import { Dimensions, Image, TextInput } from 'react-native';
 import { signIn } from '../actions/authActions';
 import { connect } from 'react-redux'
+import glamorous from "glamorous-native"
+
+import { appBackground, appTitle, secondary, secondaryDark, primaryDark, noteText } from '../assets/styles/colors'
 
 class Login extends React.Component {
     constructor(props) {
@@ -28,18 +31,82 @@ class Login extends React.Component {
 
     render() {
         return (
-            <View>
-                <Text>Login</Text>
-                <TextInput
-                    style={{ borderColor: 'black', borderWidth: 1 }}
-                    onChangeText={(text) => this.setState({ text })}
-                    value={this.state.text}
-                />
-                <TouchableHighlight onPress={this.handlePress}><Text>Submit</Text></TouchableHighlight>
-            </View>
+            <PostsView>
+                <TitleView>
+                    <TitleText>Yikyok </TitleText>
+                </TitleView>
+                <PhoneView>
+                    <PhoneInputText>Enter your phone number</PhoneInputText>
+                    <PhoneInputView>
+                        <TextInput
+                            style={{ borderColor: 'black', borderWidth: 1 }}
+                            onChangeText={(text) => this.setState({ text })}
+                            value={this.state.text}
+                        />
+                    </PhoneInputView>
+                </PhoneView>
+                <SubmitView>
+                    <SubmitButton
+                        onPress={this.handlePress}
+                        underlayColor={secondaryDark}
+                    >
+                        <Image
+                            source={ require('../assets/icons/signInIcon.png') }
+                        />
+                    </SubmitButton>
+                </SubmitView>
+            </PostsView>
         );
     }
 }
+
+const PostsView = glamorous.view({
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: appBackground,
+})
+
+const TitleText = glamorous.text({
+    color: appTitle,
+    fontSize: 45,
+    fontWeight: 'bold',
+    top: '50%'
+})
+
+const TitleView = glamorous.view({
+    flex: 0.3,
+    alignItems: 'center',
+})
+
+const PhoneInputText = glamorous.text({
+    width: "95%",
+    color: noteText
+})
+
+const PhoneInputView = glamorous.textInput({
+    width: "95%",
+    borderWidth: 3,
+    borderColor: secondary,
+    borderRadius: 6
+})
+
+const PhoneView = glamorous.view({
+    flex: 0.2,
+    justifyContent: 'center',
+    alignItems: 'center'
+})
+
+const SubmitButton = glamorous.touchableHighlight({
+    top: "5%",
+    backgroundColor: secondary,
+    borderRadius: Dimensions.get("window").width,
+    padding: 10
+})
+
+const SubmitView = glamorous.view({
+    flex: 0.5,
+    alignItems: 'center'
+})
 
 const mapStateToProps = (state) => {
     return ({
